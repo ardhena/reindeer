1b56f64a2aff3166e35bf7217218c80f2257c328
ardhena <olgadraber@gmail.com>
2014-10-09 12:14:21 +0200

    migration - add admin_id to articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002102058) do
+ActiveRecord::Schema.define(version: 20141009100615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20141002102058) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "admin_id"
   end
 
 end
