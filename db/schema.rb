fcbcbb4e2673008ef17476415dbbe8c879070d98
ardhena <olgadraber@gmail.com>
2014-10-23 09:50:05 +0200

    migration AddNamesToAmins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141023074219) do
+ActiveRecord::Schema.define(version: 20141023074843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20141023074219) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "country"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
