6bf6dbf0ca1a5b692ec7ac66cd89b349ee01cff9
Olga <olgadraber@gmail.com>
2014-11-06 08:58:00 +0100

    spec for article model - default scope

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -14,4 +14,14 @@ describe Article do
 			expect(Article.desc_by_date).to eq([n1, n2])
 		end
 	end
+
+	describe 'default scope' do
+		it 'orders all articles desc by created_at' do
+			n1 = Article.create!
+			n2 = Article.create!
+			n2.created_at = Time.now - 2.day
+			n2.save!
+			expect(Article.all).to eq([n1, n2])
+		end
+	end
 end
