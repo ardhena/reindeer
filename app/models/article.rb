1ea4511b1d38a854dd7db1ee0bc6fd25d7b933ee
Olga <olgadraber@gmail.com>
2014-11-06 13:05:56 +0100

    add article model validations + spec

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
   belongs_to :admin
 
+  validates :title, presence: true
+  validates :content, presence: true
+
   scope :desc_by_date, -> { order("created_at desc") }
   default_scope { desc_by_date }
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe Article do
 	describe 'model relations' do
 		it { should belong_to(:admin) }
+		it { should validate_presence_of :title }
+		it { should validate_presence_of :content }
 	end
 
-	let!(:a1) { Article.create(created_at: Time.now - 2.days) }
-	let!(:a2) { Article.create }
+	let!(:a1) { Article.create(title: 'foo', content: 'bar', created_at: Time.now - 2.days) }
+	let!(:a2) { Article.create(title: 'foo', content: 'bar') }
 
 	describe '#desc_by_date' do
 		it 'returns all articles in descending order by created_at' do
