31f2289e1dc7f73ba859a66c763d322b11ef51ce
Olga <olgadraber@gmail.com>
2014-11-06 09:02:53 +0100

    in article model spec let! articles to be DRY

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,23 +5,18 @@ describe Article do
 		it { should belong_to(:admin) }
 	end
 
+	let!(:n1) { Article.create(created_at: Time.now - 2.days) }
+	let!(:n2) { Article.create }
+
 	describe '#desc_by_date' do
 		it 'returns all articles in descending order by created_at' do
-			n1 = Article.create!
-			n2 = Article.create!
-			n2.created_at = Time.now - 2.day
-			n2.save!
-			expect(Article.desc_by_date).to eq([n1, n2])
+			expect(Article.desc_by_date).to eq([n2, n1])
 		end
 	end
 
 	describe 'default scope' do
 		it 'orders all articles desc by created_at' do
-			n1 = Article.create!
-			n2 = Article.create!
-			n2.created_at = Time.now - 2.day
-			n2.save!
-			expect(Article.all).to eq([n1, n2])
+			expect(Article.all).to eq([n2, n1])
 		end
 	end
 end
