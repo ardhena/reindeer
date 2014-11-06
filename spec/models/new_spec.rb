ce52c2ea5fcab678884dc24fe6910b4dca17921c
Olga <olgadraber@gmail.com>
2014-11-06 13:07:47 +0100

    add news model validations + spec

--- spec/models/new_spec.rb
+++ spec/models/new_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe New do
 	describe 'model relations' do
 		it { should belong_to(:admin) }
+		it { should validate_presence_of :title }
+		it { should validate_presence_of :content }
 	end
 
-	let!(:n1) { New.create(created_at: Time.now - 2.days) }
-	let!(:n2) { New.create }
+	let!(:n1) { New.create(title: 'foo', content: 'bar', created_at: Time.now - 2.days) }
+	let!(:n2) { New.create(title: 'foo', content: 'bar') }
 
 	describe '#desc_by_date' do
 		it 'returns all news in descending order by created_at' do
