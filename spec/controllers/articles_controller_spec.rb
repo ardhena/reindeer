65b9fdc2bee8f43a479ff6a150653df1c4d56af9
Olga <olgadraber@gmail.com>
2014-11-06 12:01:57 +0100

    fix mixing of controllers

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,32 +1,27 @@
 require 'rails_helper'
 
-describe PagesController do
-	describe 'GET home' do
+describe ArticlesController do
+	describe 'GET index' do
 		it 'has a 200 status code' do
-			get :home
+			get :index
 			expect(response.status).to eq(200)
 		end
 
 		context 'when articles exist' do
 			it 'exposes all articles' do
 				a = Article.create!
-				get :home
+				get :index
 				expect(assigns(:articles)).to eq([a])
 			end
 		end
 
 		context 'when articles is nil' do
 			it 'exposes no articles' do
-				get :home
+				get :index
 				expect(assigns(:articles)).to eq([])
 			end
 		end
 	end
 
-	describe 'GET about' do
-		it 'has a 200 status code' do
-			get :about
-			expect(response.status).to eq(200)
-		end
-	end
+	describe 'GET show'
 end
