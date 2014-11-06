62f4eaec1bf896be8dfe7ec5f5d54615ae148d69
Olga <olgadraber@gmail.com>
2014-11-06 08:53:43 +0100

    add article model spec - scope :desc_by_date

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,4 +4,14 @@ describe Article do
 	describe 'model relations' do
 		it { should belong_to(:admin) }
 	end
+
+	describe '#desc_by_date' do
+		it 'returns all articles in descending order by created_at' do
+			n1 = Article.create!
+			n2 = Article.create!
+			n2.created_at = Time.now - 2.day
+			n2.save!
+			expect(Article.desc_by_date).to eq([n1, n2])
+		end
+	end
 end
