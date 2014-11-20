37c275c13377d1c1c879dc381662db208562a05f
Olga <olgadraber@gmail.com>
2014-11-20 11:51:43 +0100

    expose decorated articles and news in pages#home view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
-	expose(:articles) { Article.limit(1) }
-	expose(:news) { New.limit(3) }
+	expose_decorated(:articles) { Article.limit(1) }
+	expose_decorated(:news, decorator: NewDecorator, collection: true) { New.limit(3) }
 
   def home
   end
