a6caa1f094dcd181884600a162cd99815b853a40
Olga <olgadraber@gmail.com>
2014-11-20 13:04:27 +0100

    migration - add info about users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141023101853) do
+ActiveRecord::Schema.define(version: 20141120115916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,10 @@ ActiveRecord::Schema.define(version: 20141023101853) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "country"
+    t.date     "birth"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
