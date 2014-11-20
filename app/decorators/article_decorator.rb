78a0b7b39361e6557fddaf80988965ba9741fcd1
Olga <olgadraber@gmail.com>
2014-11-20 09:19:58 +0100

    create article decorator

---
+++ app/decorators/article_decorator.rb
@@ -0,0 +1,11 @@
+class ArticleDecorator < Draper::Decorator
+	delegate :title, :content
+
+	def date
+		object.created_at.to_formatted_s(:db)
+	end
+
+	def author
+		object.admin.first_name + ' ' + object.admin.last_name
+	end
+end 
