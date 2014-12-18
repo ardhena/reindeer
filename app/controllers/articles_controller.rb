eefa331e9984a189a9cb8d67e7bd66b6278ef71f
ardhena <olgadraber@gmail.com>
2014-12-18 09:16:02 +0100

    add pagination to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-	expose_decorated(:articles)
+	expose_decorated(:articles) { Article.all.page(params[:page]).per(2) }
 	expose_decorated(:article) { Article.find(params[:id]) }
 
   def index
