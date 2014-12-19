da0e43e5d18316c689787046c8e9258b30ed89fd
ardhena <olgadraber@gmail.com>
2014-12-19 17:25:24 +0100

    set minimum length for news and article content

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
   belongs_to :admin
 
   validates :title, presence: true
-  validates :content, presence: true
+  validates :content, presence: true, length: { minimum: 2000 }
 
   scope :desc_by_date, -> { order("created_at desc") }
   default_scope { desc_by_date }
--- app/models/new.rb
+++ app/models/new.rb
@@ -2,7 +2,7 @@ class New < ActiveRecord::Base
   belongs_to :admin
 
   validates :title, presence: true
-  validates :content, presence: true
+  validates :content, presence: true, length: { minimum: 320 }
 
   scope :desc_by_date, -> { order("created_at desc") }
   default_scope { desc_by_date }
