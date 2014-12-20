69fd4d1ffc591998e7e75251185cfa04ad9ae3b2
ardhena <olgadraber@gmail.com>
2014-12-20 23:51:19 +0100

    complete articles controller spec

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,26 +2,34 @@ require 'rails_helper'
 
 describe ArticlesController do
 	describe 'GET index' do
-		it 'has a 200 status code' do
+		it 'exposes all articles' do
+			article = create(:article)
 			get :index
-			expect(response.status).to eq(200)
+			expect(controller.articles).to eq([article])
 		end
-
-		context 'when articles exist' do
-			it 'exposes all articles' do
-				a = Article.create!
-				get :index
-				expect(assigns(:articles)).to eq([a])
-			end
+		it 'paginates articles' do
+			a1 = create(:article)
+			a2 = create(:article)
+			a3 = create(:article)
+			a4 = create(:article)
+			get :index
+			expect(controller.articles.page(1).per(3)).to match_array([a4, a3, a2])
 		end
-
-		context 'when articles is nil' do
-			it 'exposes no articles' do
-				get :index
-				expect(assigns(:articles)).to eq([])
-			end
+		it 'renders index template' do
+			get :index
+			expect(response).to render_template(:index)
 		end
 	end
 
-	describe 'GET show'
+	describe 'GET show' do
+		let(:article) { create(:article) }
+		it 'exposes an article' do
+			get :show, id: article
+			expect(controller.article).to eq(article)
+		end
+		it 'renders show template' do
+			get :show, id: article
+			expect(response).to render_template(:show)
+		end
+	end
 end
