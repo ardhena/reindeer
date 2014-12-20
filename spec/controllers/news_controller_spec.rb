991c435c3e14c215e8244f1d57ef0ebdddf443ac
ardhena <olgadraber@gmail.com>
2014-12-20 23:53:32 +0100

    complete news controller spec

--- spec/controllers/news_controller_spec.rb
+++ spec/controllers/news_controller_spec.rb
@@ -2,26 +2,36 @@ require 'rails_helper'
 
 describe NewsController do
 	describe 'GET index' do
-		it 'has a 200 status code' do
+		it 'exposes all news' do
+			new = create(:new)
 			get :index
-			expect(response.status).to eq(200)
+			expect(controller.news).to eq([new])
 		end
-
-		context 'when news exist' do
-			it 'exposes all news' do
-				n = New.create!
-				get :index
-				expect(assigns(:news)).to eq([n])
-			end
+		it 'paginates news' do
+			n1 = create(:new)
+			n2 = create(:new)
+			n3 = create(:new)
+			n4 = create(:new)
+			n5 = create(:new)
+			n6 = create(:new)
+			get :index
+			expect(controller.news.page(1).per(5)).to match_array([n6, n5, n4, n3, n2])
 		end
-
-		context 'when news is nil' do
-			it 'exposes no news' do
-				get :index
-				expect(assigns(:news)).to eq([])
-			end
+		it 'renders index template' do
+			get :index
+			expect(response).to render_template(:index)
 		end
 	end
 
-	describe 'GET show'
+	describe 'GET show' do
+		let(:new) { create(:new) }
+		it 'exposes a news' do
+			get :show, id: new
+			expect(controller.new).to eq(new)
+		end
+		it 'renders show template' do
+			get :show, id: new
+			expect(response).to render_template(:show)
+		end
+	end
 end
