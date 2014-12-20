c43b7a115506eba801bb79022a03bf49f63db695
ardhena <olgadraber@gmail.com>
2014-12-20 17:32:06 +0100

    complete news model specs

--- spec/models/new_spec.rb
+++ spec/models/new_spec.rb
@@ -5,10 +5,11 @@ describe New do
 		it { should belong_to(:admin) }
 		it { should validate_presence_of :title }
 		it { should validate_presence_of :content }
+		it { should ensure_length_of(:content).is_at_least(320) }
 	end
 
-	let!(:n1) { New.create(title: 'foo', content: 'bar', created_at: Time.now - 2.days) }
-	let!(:n2) { New.create(title: 'foo', content: 'bar') }
+	let!(:n1) { New.create(title: 'foo', content: 'bar'*160, created_at: Time.now - 2.days) }
+	let!(:n2) { New.create(title: 'foo', content: 'bar'*160) }
 
 	describe '#desc_by_date' do
 		it 'returns all news in descending order by created_at' do
