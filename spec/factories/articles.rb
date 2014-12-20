cf2becaf6ea6d89595524293efce912e0c4874ea
ardhena <olgadraber@gmail.com>
2014-12-20 18:01:08 +0100

    add factories rspec

---
+++ spec/factories/articles.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :article do
+    title 'Test article'
+    content 'a'*2000
+  end
+end
---
+++ spec/factories/news.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :new do
+    title 'Test news'
+    content 'a'*320
+  end
+end
