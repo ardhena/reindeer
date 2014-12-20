4bddf2afdbadb4a8d60b644d16951b695259f0fa
ardhena <olgadraber@gmail.com>
2014-12-20 13:53:07 +0100

    set news and articles per page, center pagination links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-	expose_decorated(:articles) { Article.all.page(params[:page]).per(5) }
+	expose_decorated(:articles) { Article.all.page(params[:page]).per(3) }
 	expose_decorated(:article) { Article.find(params[:id]) }
 
   def index
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,7 +1,7 @@
 class NewsController < ApplicationController
 
 	expose_decorated(:news, decorator: NewDecorator, collection: true) {
-    New.all.page(params[:page]).per(2) }
+    New.all.page(params[:page]).per(5) }
 	expose_decorated(:new, decorator: NewDecorator, collection: false) { New.find(params[:id]) }
 
   def index
