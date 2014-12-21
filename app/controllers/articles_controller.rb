baee4d1e7fea467463d568f4c472fd06b953b06a
ardhena <olgadraber@gmail.com>
2014-12-21 15:57:43 +0100

    add comments form and display to articles#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,8 @@ class ArticlesController < ApplicationController
 
 	expose_decorated(:articles) { Article.all.page(params[:page]).per(3) }
 	expose_decorated(:article) { Article.find(params[:id]) }
+  expose_decorated(:comments) { article.comments }
+  expose_decorated(:new_comment, decorator: CommentDecorator) { article.comments.new }
 
   def index
   end
