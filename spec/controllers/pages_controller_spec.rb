357083a703fdeaf281b01f48a1f4c898e28f9d85
ardhena <olgadraber@gmail.com>
2014-12-21 11:25:16 +0100

    complete pages controller spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,51 +2,30 @@ require 'rails_helper'
 
 describe PagesController do
 	describe 'GET home' do
-		it 'has a 200 status code' do
+		it 'exposes 3 latest news' do
+			n1 = create(:new)
+			n2 = create(:new)
+			n3 = create(:new)
+			n4 = create(:new)
 			get :home
-			expect(response.status).to eq(200)
+			expect(controller.news).to eq([n4, n3, n2])
 		end
-
-		context 'when news exist' do
-			it 'exposes all news' do
-				n1 = New.create!
-				n2 = New.create!
-				n3 = New.create!
-				n4 = New.create!
-				get :home
-				expect(assigns(:news)).to eq([n4, n3, n2])
-			end
+		it 'exposes 1 latest article' do
+			a1 = create(:article)
+			a2 = create(:article)
+			get :home
+			expect(controller.articles).to eq([a2])
 		end
-
-		context 'when news is nil' do
-			it 'exposes no news' do
-				get :home
-				expect(assigns(:news)).to eq([])
-			end
+		it 'renders home template' do
+			get :home
+			expect(response).to render_template(:home)
 		end
-
-		context 'when articles exist' do
-			it 'exposes all articles' do
-				a1 = Article.create!
-				a2 = Article.create!
-				get :home
-				expect(assigns(:articles)).to eq([a2])
-			end
-		end
-
-		context 'when articles is nil' do
-			it 'exposes no articles' do
-				get :home
-				expect(assigns(:articles)).to eq([])
-			end
-		end
-
 	end
 
 	describe 'GET about' do
-		it 'has a 200 status code' do
+		it 'renders about template' do
 			get :about
-			expect(response.status).to eq(200)
+			expect(response).to render_template(:about)
 		end
 	end
 end
