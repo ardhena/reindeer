8398454dd54194c17cef4cc3937e9b671bc002a1
ardhena <olgadraber@gmail.com>
2014-12-21 12:32:46 +0100

    add routes for comments (create, destroy)

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   resources :news, only: [:index, :show]
   get 'pages/about', as: 'about'
   resources :profiles
+  resources :comments, only: [:create, :destroy]
 end
