2cbaaff08fd8018cd5647413701f6db3d3109370
ardhena <olgadraber@gmail.com>
2014-12-21 14:48:23 +0100

    render comments form and comments in news show

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,6 +3,8 @@ class NewsController < ApplicationController
 	expose_decorated(:news, decorator: NewDecorator, collection: true) {
     New.all.page(params[:page]).per(5) }
 	expose_decorated(:new, decorator: NewDecorator, collection: false) { New.find(params[:id]) }
+  expose_decorated(:comments) { new.comments }
+  expose_decorated(:new_comment, decorator: CommentDecorator) { new.comments.new }
 
   def index
   end
