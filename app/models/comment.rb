6fdac1abf544b045f0b3f29a68bf83d8d553fd9a
ardhena <olgadraber@gmail.com>
2014-12-21 14:48:59 +0100

    add validations to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,15 @@
 class Comment < ActiveRecord::Base
-
   include ActsAsCommentable::Comment
 
-  belongs_to :commentable, :polymorphic => true
-
-  default_scope -> { order('created_at ASC') }
-
-  # NOTE: install the acts_as_votable plugin if you
-  # want user to vote on the quality of comments.
-  #acts_as_voteable
-
-  # NOTE: Comments belong to a user
+  belongs_to :commentable, polymorphic: true
   belongs_to :user
+
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :comment, presence: true, length: { minimum: 10 }
+  validates :commentable_id, presence: true
+  validates :commentable_type, presence: true
+  validates :user_id, presence: true
+
+  default_scope -> { order('created_at desc') }
+
 end
