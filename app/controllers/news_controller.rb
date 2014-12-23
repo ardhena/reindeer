7dede7b931d9f1c69198a8b748803f311edb3da9
ardhena <olgadraber@gmail.com>
2014-12-23 21:11:15 +0100

    update comments for news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,7 +4,7 @@ class NewsController < ApplicationController
     New.all.page(params[:page]).per(5) }
 	expose_decorated(:new, decorator: NewDecorator, collection: false) { New.find(params[:id]) }
   expose_decorated(:comments) { new.comments }
-  expose_decorated(:new_comment, decorator: CommentDecorator) { new.comments.new }
+  expose(:object) { new.class }
 
   def index
   end
