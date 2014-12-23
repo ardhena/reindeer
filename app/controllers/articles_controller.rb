5db079b14fffd56bddb4a4b0c4a190c962ca217d
ardhena <olgadraber@gmail.com>
2014-12-23 20:10:16 +0100

    rewrite create comment action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,6 @@ class ArticlesController < ApplicationController
 	expose_decorated(:article) { Article.find(params[:id]) }
   expose_decorated(:comments) { article.comments }
   expose(:object) { article.class }
-  # expose_decorated(:new_comment, decorator: CommentDecorator) { article.comments.new }
 
   def index
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,12 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
+  expose(:object) { comment_params[:commentable_type].constantize.find(comment_params[:commentable_id]) }
+  expose_decorated(:comments) { object.comments }
+  expose_decorated(:comment) { comments.first }
+
   def new
-    @object = object_params[:type].constantize.find(object_params[:id])
-    @new_comment = @object.comments.new
+    @new_comment = object.comments.new
     respond_to do |format|
       format.js
     end
@@ -16,22 +19,24 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @object = comment_params[:commentable_type].constantize.find(comment_params[:commentable_id])
-    @comment = @object.comments.create(comment_params)
+    @comment = object.comments.create(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-      render partial: "comments/comment", locals: { comment: @comment.decorate }, layout: false, status: :created
+      respond_to do |format|
+        format.js
+      end
+      # render partial: "comments/comment", locals: { comment: @comment.decorate }, layout: false, status: :created
     else
-      render js: "alert('error saving comment');"
+      respond_to do |format|
+        format.js   { render action: "new" }
+        format.json { render json: @comment }
+      end
+      # render js: "alert('error saving comment');"
     end
     # <div class="alert alert-danger">...</div>
   end
 
   def destroy
-  end
-
-  def object_params
-    params.require(:object).permit(:type, :id)
   end
 
   def comment_params
