a37b93db0f4bb2fbb950b64d9b7187914d3ab4a8
ardhena <olgadraber@gmail.com>
2014-12-27 11:34:07 +0100

    add enums and model validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  enum country: [:norway, :poland]
+  enum school: [:zsk, :nschool]
+  enum sex: [:female, :male]
+
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :birth, presence: true
+  validates :country, presence: true
+  validates :school, presence: true
+  validates :sex, presence: true
+  validates :city, presence: true, length: { minimum: 2 }
 end
