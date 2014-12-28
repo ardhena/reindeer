46ef8804d90506c4cd6ed9833b56d594e65620d4
ardhena <olgadraber@gmail.com>
2014-12-28 19:16:55 +0100

    add user gravatar, style user right navbar dropdown

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  include Gravtastic
+  has_gravatar default: :identicon, size: 40
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
