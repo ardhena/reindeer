9865c1d6d93ce0ca01f6957a7479d45ac4c7fb86
ardhena <olgadraber@gmail.com>
2015-01-27 14:33:27 +0100

    after sending or accepting friend request redirect to notifications friend requests

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,7 +5,7 @@ class FriendshipsController < ApplicationController
     if current_user.has_no_friendship_with?(@user)
       friendship = Friendship.new(user_id: current_user.id, friend_id: @user.id)
       if friendship.save
-        redirect_to profile_path(@user.id), notice: 'Your friend request was send.'
+        redirect_to notifications_friend_requests_path, notice: 'Your friend request was send.'
       end
     end
   end
@@ -17,7 +17,7 @@ class FriendshipsController < ApplicationController
       f2 = Friendship.new(user_id: current_user.id, friend_id: @user.id, accepted: true)
       if f2.save
         f1.update(accepted: true)
-        redirect_to profile_path(@user.id), notice: 'You are now friends with ' + User.find(@user.id).decorate.name + '.'
+        redirect_to notifications_friend_requests_path, notice: 'You are now friends with ' + User.find(@user.id).decorate.name + '.'
       end
     end
   end
